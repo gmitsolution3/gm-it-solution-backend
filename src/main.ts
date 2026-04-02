6fb932616517af66f9a19c4d0234bb97411acb9e
gmitsolution3 <gmitsolution3@gmail.com>
2026-04-02 11:18:01 +0600

    added options on cors method

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
   const corsOptions: CorsOptions = {
     origin: ['https://www.gmitsolution.net', 'http://localhost:5173'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
   };
